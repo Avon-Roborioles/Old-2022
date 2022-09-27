08e5006d1d15a467b213ac48b0161fc940d066c2
Stephen O <87861480+Stephen-Onochie@users.noreply.github.com>
2022-09-27 16:07:56 -0700

    Added Team 14 Autonomous Folder
    A filler file is included to create the folder :)

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/14Autonomous/Filler.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import java.util.ArrayList;
+import org.firstinspires.ftc.teamcode.State;
+import org.firstinspires.ftc.teamcode.StateController;
+
+//Drives straight to warehouse area
+
+@Autonomous
+public class Filler {
+  
+}
