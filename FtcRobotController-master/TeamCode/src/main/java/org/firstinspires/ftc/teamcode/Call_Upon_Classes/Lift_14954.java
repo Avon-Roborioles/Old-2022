604b8c8ef775d414ad22225a85df6203a8b81a5b
Stephen-Onochie <stephenconochie@gmail.com>
2022-09-29 19:06:26 -0700

    Added initial Lift Program

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/Lift_14954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/Lift_14954.java
@@ -1,9 +1,37 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Call_Upon_Classes;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Lift_14954 {
+    private double speed = 0;
+
+    private DcMotor lift = null;
+
+    public void init_lift (HardwareMap map, String name) {
+        lift  = map.get(DcMotor.class, name);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    //simple arm program to move arm up will work on position holding soon
+    public void runlift(Gamepad gp, Telemetry telemetry) {
+        double ltrigger = gp.left_trigger;
+        double rtrigger = gp.right_trigger;
+
+        if (ltrigger > 0) {
+            speed = .6;
+        } else if (rtrigger > 0 ) {
+            speed = -.6;
+        } else {
+            speed = 0;
+        }
+
+        lift.setPower(speed);
+
+    }
+
+
 }
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
@@ -1,20 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+//import classes for objects;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_TeleOp;
-
+import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954;
 
 @TeleOp
 public class TeleOp_14954 extends LinearOpMode {
 //objects for each function of the robot
 private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_TeleOp drivingMotors = new Mecanum_Methods_TeleOp(false);
-
+private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954 lift = new Lift_14954();
 
 public void runOpMode() throws InterruptedException {
     //Initialize objects
     drivingMotors.init_drive_motors(hardwareMap);
-
+    lift.init_lift(hardwareMap, "lift");
 
     waitForStart();
 
@@ -22,8 +23,8 @@ public void runOpMode() throws InterruptedException {
         //Driver 1 Functions
         drivingMotors.run_drive_motors(gamepad1, telemetry); //driving
 
-
         //Driver 2 Functions
+        lift.runlift(gamepad2, telemetry);
 
         telemetry.update();
         }
