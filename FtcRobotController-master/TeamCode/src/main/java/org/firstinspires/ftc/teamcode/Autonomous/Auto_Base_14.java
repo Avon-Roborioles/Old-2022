d366ed589d7d83e2dc1277383ccaa82c4543cda0
Stephen-Onochie <stephenconochie@gmail.com>
2022-10-04 19:40:52 -0700

    Added Files for 14954
    Added Camera, intake, etc. classes

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_Base_14.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_Base_14.java
@@ -2,14 +2,31 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Mecanum_IMU;
+import org.firstinspires.ftc.teamcode.Mecanum_Methods_Autonomus;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954;
-import org.firstinspires.ftc.teamcode.Mecanum_Methods_Autonomus;
-import org.firstinspires.ftc.teamcode.Lift_14954;
-import org.firstinspires.ftc.teamcode.Intake_14954;
+import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Camera;
 
 public abstract class Auto_Base_14 extends LinearOpMode {
     protected org.firstinspires.ftc.teamcode.Mecanum_Methods_Autonomus auto_motors = new Mecanum_Methods_Autonomus();
-    protected org.firstinspires.ftc.teamcode.Lift_14954 lift = new Lift_14954();
-    protected org.firstinspires.ftc.teamcode.Intake_14954 intake = new Intake_14954();
+    protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954 lift = new Lift_14954();
+    protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954 intake = new Intake_14954();
+    protected org.firstinspires.ftc.teamcode.Mecanum_IMU imu_drive = new Mecanum_IMU();
+    protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Camera camera = new Camera();
+
+    protected int inchToTicks = 91;
+    protected int scorePosition;
+
+    public void init_classes(boolean red_alliance) {
+        auto_motors.init_auto_drive_motors(hardwareMap, telemetry);
+        lift.init_lift(hardwareMap, "lift");
+        intake.init_intake(hardwareMap, "intake");
+        imu_drive.init_drive_motors(hardwareMap, telemetry,false);
+        camera.
+    }
+
+    public void sense_cone (){
+        //use camera to scan cone images
+    }
 }
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 //import classes for objects;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_TeleOp;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954;
@@ -12,13 +14,14 @@ public class TeleOp_14954 extends LinearOpMode {
 //objects for each function of the robot
 private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_TeleOp drivingMotors = new Mecanum_Methods_TeleOp(false);
 private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954 lift = new Lift_14954();
-
+private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954 intake = new Intake_14954();
 
 
 public void runOpMode() throws InterruptedException {
     //Initialize objects
     drivingMotors.init_drive_motors(hardwareMap);
     lift.init_lift(hardwareMap, "lift");
+    intake.init_intake(hardwareMap, "intake");
 
     waitForStart();
 
@@ -28,6 +31,7 @@ public void runOpMode() throws InterruptedException {
 
         //Driver 2 Functions
         lift.runlift(gamepad2, telemetry);
+        intake.run_intake(gamepad2, telemetry);
 
         telemetry.update();
         }
