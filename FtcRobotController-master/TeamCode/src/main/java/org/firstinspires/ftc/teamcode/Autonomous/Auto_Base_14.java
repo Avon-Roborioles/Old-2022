38b15b3d5090a9e5cb4e560a20ecff279fe1a842
Stephen-Onochie <stephenconochie@gmail.com>
2022-10-04 18:14:51 -0700

    Work on Team 14 Code

---
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_Base_14.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_Autonomous;
+
+
+public class Auto_Base_14 extends LinearOpMode {
+
+
+}
