75aa2a8d3f2e3616b37a4e77cf998a1143875ac4
FTCRoborioles <FTCRoborioles2021@gmail.com>
2022-10-27 18:23:31 -0400

    Tommy changes

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/Lift_21954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Call_Upon_Classes/Lift_21954.java
@@ -9,19 +9,26 @@ public class Lift_21954 {
     private double lp = 0;
     //lift motor
     private DcMotor lm = null;
-    //button a
-    private boolean a = false;
+    //Right bumper
+    private boolean rb = false;
+    //left bumper
+    private boolean lb = false;
     private void init_lift_motor_21954(HardwareMap hardwareMap) {
         lm = hardwareMap.get(DcMotor.class, "lm");
     }
     private void run_lift_motor_21954(Gamepad gamepad, Telemetry telemetry) {
-        a = gamepad.a;
-        if(a) {
+        rb = gamepad.right_bumper;
+        lb = gamepad.left_bumper;
+        if(rb) {
             lp = .4;
+        }
+        else if(lb) {
+            lp = -.4;
         }
         else {
             lp = 0;
         }
+
         lm.setPower(lp);
 
     }
--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
@@ -23,7 +23,7 @@ public void runOpMode() throws InterruptedException {
 
     while (opModeIsActive()) {
         //Driver 1 Functions
-        drivingMotors.run_drive_motors(gamepad1, telemetry); //driving
+        drivingMotors.run_drive_motors_cardinal(gamepad1, telemetry); //driving
 
         //Driver 2 Functions
         lift.runlift(gamepad2, telemetry);
