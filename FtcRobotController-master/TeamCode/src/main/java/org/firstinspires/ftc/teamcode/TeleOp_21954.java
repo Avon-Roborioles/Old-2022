ad0ba5887c2ba07e41172428a7002e986776e219
FTCRoborioles <FTCRoborioles2021@gmail.com>
2022-10-28 10:19:37 -0400

    21954 fixes

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_21954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_21954.java
@@ -17,12 +17,13 @@ import org.firstinspires.ftc.teamcode.Call_Upon_Classes.*;
 public class TeleOp_21954 extends LinearOpMode {
     //objects for each function of the robot
     private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_TeleOp drivingMotors = new Mecanum_Methods_TeleOp(false);
-    private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954 lift = new Lift_14954();
+    private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_21954 lift = new Lift_21954();
+    private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_21954 intake = new Intake_21954();
 
     public void runOpMode() throws InterruptedException {
         //Initialize objects
         drivingMotors.init_drive_motors(hardwareMap);
-//    lift.init_lift(hardwareMap, "lift");
+        lift.init_lift(hardwareMap, "lift");
 
         waitForStart();
 
@@ -33,7 +34,8 @@ public class TeleOp_21954 extends LinearOpMode {
             drivingMotors.run_drive_motors(gamepad1, telemetry); //driving
 
             //Driver 2 Functions
-//        lift.runlift(gamepad2, telemetry);
+            lift.runlift(gamepad2, telemetry);
+            intake.runintake(gamepad2, telemetry);
 
             telemetry.update();
         }
