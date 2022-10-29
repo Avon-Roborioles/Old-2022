236b82c6d36affb84b64e4a82cbc8696fe960d17
Stephen-Onochie <stephenconochie@gmail.com>
2022-10-29 12:21:17 -0700

    Implemented Turntable Code - 14

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_14954.java
@@ -10,6 +10,7 @@ public class TeleOp_14954 extends LinearOpMode {
 //objects for each function of the robot
 private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_TeleOp drivingMotors = new Mecanum_Methods_TeleOp(false);
 private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954 lift = new Lift_14954();
+private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Turntable_14954 table = new Turntable_14954();
 //private final org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954 intake = new Intake_14954();
 
 
@@ -17,6 +18,7 @@ public void runOpMode() throws InterruptedException {
     //Initialize objects
     drivingMotors.init_drive_motors(hardwareMap);
     lift.init_lift(hardwareMap, "lift");
+    table.init_turntable_14954(hardwareMap, "table");
 //    intake.init_intake(hardwareMap, "intake");
 
     waitForStart();
@@ -24,6 +26,7 @@ public void runOpMode() throws InterruptedException {
     while (opModeIsActive()) {
         //Driver 1 Functions
         drivingMotors.run_drive_motors_cardinal(gamepad1, telemetry); //driving
+        table.run_turntable_14954(gamepad1, telemetry);
 
         //Driver 2 Functions
         lift.runlift(gamepad2, telemetry);
