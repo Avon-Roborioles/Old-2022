fce143995e9410fe2d447dde3467318f022b74fa
Stephen-Onochie <stephenconochie@gmail.com>
2022-11-08 19:11:16 -0800

    Quick Changes

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_Base_14.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Auto_Base_14.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.*;
+
 
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Camera_14954;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Lift_14954;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_IMU;
 import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_Autonomus;
+import org.firstinspires.ftc.teamcode.Call_Upon_Classes.Turntable_14954;
 
 public abstract class Auto_Base_14 extends LinearOpMode {
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_Methods_Autonomus auto_motors = new Mecanum_Methods_Autonomus();
@@ -14,15 +17,18 @@ public abstract class Auto_Base_14 extends LinearOpMode {
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Intake_14954 intake = new Intake_14954();
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Mecanum_IMU imu_drive = new Mecanum_IMU();
     protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Camera_14954 camera = new Camera_14954();
+    protected org.firstinspires.ftc.teamcode.Call_Upon_Classes.Turntable_14954 table = new Turntable_14954();
 
     protected int inchToTicks = 91;
     protected int scorePosition;
 
     public void init_classes(boolean red_alliance) {
-//        auto_motors.init_auto_drive_motors(hardwareMap, telemetry);
+        auto_motors.init_auto_drive_motors(hardwareMap, telemetry);
         lift.init_lift(hardwareMap, "lift");
-//        intake.init_intake(hardwareMap, "intake");
-//        imu_drive.init_drive_motors(hardwareMap, telemetry, false);
+        intake.init_intake(hardwareMap, "intake");
+        imu_drive.init_drive_motors(hardwareMap, telemetry, false);
+        table.init_turntable_14954(hardwareMap, "table");
+        camera.init();
     }
 
 
